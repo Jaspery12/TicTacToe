2b311b57b7e862ce776ef7835c0b1f5b46c92522
Jaspery12 <jaspery12@gmail.com>
2025-07-21 17:14:08 +0800

    Added boxes/buttons and the panel to play

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,21 +1,66 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.swing.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class TicTacToe {
   int boardWidth = 600;
   int boardHeight = 650;
 
-  JFrame frame = new JFrame();
+  JFrame frame = new JFrame("Tic Tac Toe");
+  JLabel textLabel = new JLabel();
+  JPanel textPanel = new JPanel();
+  JPanel boardPanel = new JPanel();
+  
+  JButton[][] board = new JButton[3][3];
+  String playerX = "X";
+  String playerO = "O";
+  String currentPlayer = playerX;
 
   public TicTacToe() {
     frame.setVisible(true);
-    frame.setSize(boardHeight,boardHeight);
+    frame.setSize(boardWidth,boardHeight);
     frame.setLocationRelativeTo(null);
     frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
+
+    textLabel.setBackground(Color.BLACK);
+    textLabel.setForeground(Color.WHITE);
+    textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+    textLabel.setHorizontalAlignment(JLabel.CENTER);
+    textLabel.setText("Tic Tac Toe");
+    textLabel.setOpaque(true);
+
+    textPanel.setLayout(new BorderLayout());
+    textPanel.add(textLabel);
+    frame.add(textPanel, BorderLayout.NORTH);
+
+    boardPanel.setLayout(new GridLayout(3,3));
+    boardPanel.setBackground(Color.gray);
+    frame.add(boardPanel);
+
+    for(int r=0; r<3; r++){
+      for(int c=0; c<3; c++){
+        JButton box = new JButton();
+        board[r][c] = box;
+        boardPanel.add(box);
+
+        box.setBackground(Color.gray);
+        box.setForeground(Color.white);
+        box.setFont(new Font("Arial", Font.BOLD, 120));
+        box.setFocusable(false); //So it can only be clicked with mouse 
+
+        box.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e){
+            JButton box = (JButton) e.getSource(); //Type button as there is only buttons 
+            box.setText(currentPlayer);
+
+            currentPlayer = currentPlayer == playerX ? playerO : playerX;
+            textLabel.setText(currentPlayer + "'s turn");
+          }
+        });
+      }
+    }
   }
   
 }
